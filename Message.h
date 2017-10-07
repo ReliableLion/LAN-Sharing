8287388dbbdba10cac18613a7d0cf1d1faf724ad
Davide Maraschio <davide.maraschio93@gmail.com>
2017-10-07 22:06:15 +0200

    Addedd protocol message

---
+++ Message.h
@@ -0,0 +1,52 @@
+
+
+#pragma once
+
+#include <iostream>
+#include <windows.h>
+#include <string>
+#include <sstream>
+
+class Message {
+
+public:
+	std::string getMessageBody() {
+		return messageBody;
+	}
+
+protected:
+	std::string messageBody;
+	std::stringstream stream;
+	const std::string endMessage = "\r\n";
+	ULARGE_INTEGER timeStamp;
+	std::string string;
+
+};
+
+class RequestMessage : public Message {
+
+public:
+	RequestMessage(__int64 fileSize, FILETIME fileTimestamp, std::string fileName) {
+
+		messageBody.append(sendMessage);
+
+		stream << fileSize;
+		messageBody.append(stream.str());
+
+		timeStamp.LowPart = fileTimestamp.dwLowDateTime;
+		timeStamp.HighPart = fileTimestamp.dwHighDateTime;
+		stream.clear();
+		stream << timeStamp.QuadPart;
+
+		messageBody.append(stream.str());
+
+		messageBody.append(fileName);
+
+		messageBody.append(endMessage);
+
+	}
+
+private:
+	const std::string sendMessage = "SEND ";
+
+};
