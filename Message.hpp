408f165fb1ce0f1fddf739217867739046c19032
Davide Maraschio <davide.maraschio93@gmail.com>
2017-10-20 18:55:08 +0200

    fixed Message

--- Message.hpp
+++ Message.hpp
@@ -30,24 +30,43 @@ public:
 
 		messageBody.append(sendMessage);
 
-		stream << fileSize;
+		this->fileSize = fileSize;
+		this->fileTimestamp = fileTimestamp;
+		this->fileName = fileName;
+
+		stream << this->fileSize;
 		messageBody.append(stream.str());
 
-		timeStamp.LowPart = fileTimestamp.dwLowDateTime;
-		timeStamp.HighPart = fileTimestamp.dwHighDateTime;
+		timeStamp.LowPart = this->fileTimestamp.dwLowDateTime;
+		timeStamp.HighPart = this->fileTimestamp.dwHighDateTime;
 		stream.clear();
 		stream << timeStamp.QuadPart;
 
 		messageBody.append(stream.str());
 
-		messageBody.append(fileName);
+		messageBody.append(this->fileName);
 
 		messageBody.append(endMessage);
 
 	}
 
+	__int64 getFileSize() {
+		return this->fileSize;
+	}
+
+	FILETIME getFileTimeStamp() {
+		return this->fileTimestamp;
+	}
+
+	std::string getFileName() {
+		return this->fileName;
+	}
+
 private:
 	const std::string sendMessage = "SEND ";
+	__int64 fileSize;
+	FILETIME fileTimestamp;
+	std::string fileName;
 
 };
 
